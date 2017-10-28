8a60e041ca4a89bd986016c287b2f99ff404df22
carsin <carsin@users.noreply.github.com>
2017-10-27 20:39:58 -1000

    add typed js subtitle

--- src/js/index.js
+++ src/js/index.js
@@ -3,9 +3,17 @@ inView(".title").on("enter", (el) => {
     console.log("set body background to " + $(el).parent().attr("data-background"));
 });
 
-inView(".title-main").on("enter", (el) => {
-    $("body").css("background", $(el).parent().attr("data-background"));
-    console.log("set body background to " + $(el).parent().attr("data-background"));
-});
-
 console.log("js load success");
+
+var options = {
+    strings: ["> Web developer", "> Car enthusiast", "> Freelancer", "> Entrepreneur"],
+    typeSpeed: 60,
+    shuffle: true,
+    backDelay: 1550,
+    smartBackspace: true,
+    loop: true,
+    showCursor: false,
+    backSpeed: 30
+}
+
+var typed = new Typed(".sub-title", options);
