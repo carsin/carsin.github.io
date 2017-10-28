1a7e267ed08a96181746019592f76697b24cd1bd
carsin <carsin@users.noreply.github.com>
2017-10-28 07:39:04 -1000

    general: update

--- src/js/index.js
+++ src/js/index.js
@@ -3,17 +3,18 @@ inView(".title").on("enter", (el) => {
     console.log("set body background to " + $(el).parent().attr("data-background"));
 });
 
+inView(".title-main").on("enter", (el) => {
+    $("body").css("background", $(el).parent().attr("data-background"));
+    console.log("set body background to " + $(el).parent().attr("data-background"));
+});
+
+$(window).scroll(() => {
+    console.log($(document).scrollTop());
+    if ($(document).scrollTop() !== 0) {
+        $("#hero-bottom").css("opacity", 0);
+    } else {
+        $("#hero-bottom").css("opacity", 100);
+    }
+});
+
 console.log("js load success");
-
-var options = {
-    strings: ["> Web developer", "> Car enthusiast", "> Freelancer", "> Entrepreneur"],
-    typeSpeed: 60,
-    shuffle: true,
-    backDelay: 1550,
-    smartBackspace: true,
-    loop: true,
-    showCursor: false,
-    backSpeed: 30
-}
-
-var typed = new Typed(".sub-title", options);
