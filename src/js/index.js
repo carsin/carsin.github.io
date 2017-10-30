217f28263ed37f9f6c107529cca8b0c21a47d2d3
carsin <carsin@users.noreply.github.com>
2017-10-29 15:37:38 -1000

    navbar: v1

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,14 @@ inView(".fadeSectionDetector").on("exit", (el) => {
     $(el).parent().parent().parent().parent().css("transform", "translateX(-10vw)");
 });
 
+inView(".title-scrollspy").on("enter", (el) => {
+    if ($(el).hasClass("title-scrollspy")) {
+        $("#nav-items").find(".nav-item.active").removeClass("active");
+        var navEle = $("#nav-items").find("a[href='#" + $(el).attr("data-navhref") + "']");
+        navEle.addClass("active");
+    }
+});
+
 inView(".title-main").on("enter", (el) => {
     $("body").css("background", $(el).parent().attr("data-background"));
     console.log("set body background to " + $(el).parent().attr("data-background"));
@@ -30,6 +38,14 @@ $(window).scroll(() => {
     } else {
         $("#hero-text").css("opacity", 100);
     }
+});
+
+$(".nav-link").click(function(e) {
+    var href = $(this).attr("href");
+    $('html, body').stop().animate({
+        scrollTop: $(href).offset().top - $("#nav-main").height()
+    }, 300);
+    e.preventDefault();
 });
 
 console.log("js load success");
